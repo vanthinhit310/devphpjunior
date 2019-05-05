e99e13dafde613619dde5b7002708be9fcb8dc81
Le Van Thinh <vanthinhit310@gmail.com>
2019-05-05 18:18:53 +0700

    Hoàn thành chức năng viết  Log

---
+++ resources/views/errors/404_custom.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vanth
+ * Date: 5/5/2019
+ * Time: 5:44 PM
+ */
---
+++ resources/views/log-daily/log-detail.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vanth
+ * Date: 5/5/2019
+ * Time: 4:08 PM
+ */
